cdd0f75a2a7cc555e80472edf1ccd26efe7a7f90
DanielYosifov <yosifov.eood@abv.bg>
2020-05-13 09:04:51 +0300

    page transition

---
+++ js/transition.js
@@ -0,0 +1,41 @@
+$('.btn_nav').click(function () {
+    // animate content
+    $('.page__style').addClass('animate_content');
+    $('.page__description').fadeOut(100).delay(1000).fadeIn();
+
+    setTimeout(function () {
+        $('.page__style').removeClass('animate_content');
+    }, 1000);
+
+    //remove fadeIn class after 1500ms
+    setTimeout(function () {
+        $('.page__style').removeClass('fadeIn');
+    }, 1500);
+
+});
+
+// on click show page after 1500ms
+$('.home_link').click(function () {
+    setTimeout(function () {
+        $('.home').addClass('fadeIn');
+    }, 1500);
+});
+
+
+$('.portfolio_link').click(function () {
+    setTimeout(function () {
+        $('.skills').addClass('fadeIn');
+    }, 1500);
+});
+
+$('.about_link').click(function () {
+    setTimeout(function () {
+        $('.about').addClass('fadeIn');
+    }, 500);
+});
+
+$('.contact_link').click(function () {
+    setTimeout(function () {
+        $('.contact').addClass('fadeIn');
+    }, 500);
+});
